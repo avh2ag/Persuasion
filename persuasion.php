b3ebe85f628ff75d1c7c0b712755e141ea8cb7e1
helenhe <yhh9zt@virginia.edu>
2014-12-02 14:38:25 -0500

    new css layout, merged all .css files into style.css, changed/added some classes

--- persuasion.php
+++ persuasion.php
@@ -6,24 +6,19 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
     <title>Persuasion</title>
 
     <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
     <!-- Custom styles for this template -->
-    <link href="jumbotron.css" rel="stylesheet">
+    <!-- <link href="jumbotron.css" rel="stylesheet"> -->
+    <link href="style.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	
-   <!-- Bootstrap core JavaScript
-   ================================================== -->
+
    <!-- Placed at the end of the document so the pages load faster -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
    <script src="../../dist/js/bootstrap.min.js"></script>
@@ -35,107 +30,55 @@
   </head>
 
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="back2">
+
+    <div class="navbar navbar-fixed-top" role="navigation" id="back2">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Welcome to the GuestBook!</a>
+        <div class="navbar-header">          
+          <a href="persuasion.php"><img class="persuabooklogo" src="persuabook.png"></a>
         </div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav navbar-right">
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Explore the site<span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="http://www.charlottesville.org/Index.aspx?page=1725">Profile List</a></li>
-		            <li><a href="http://www.bikecharlottesville.org/">Photos</a></li>
-		            <li><a href="http://www.reelingandrafting.com/tubing.htm">Events</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-      </div>
-    </div>
+        <form class="navbar-form navbar-right" role="form" action="profile.php" method="GET">
+	        <div class="form-group">
+	          <label for="category" class="charsearch"><i class="fa fa-search fa-lg"></i></label>
+			  		<select class="form-control" id="category" name="category">
+			  			<option selected disabled hidden value=''></option>
+			      		<option value="Anne_Elliot">Anne Elliot</option>
+  						<option value="Captain_Frederick_Wentworth">Capt. Fredrick Wentworth</option>
+			     		<option value="Sir_Walter_Elliot">Sir Walter Elliot</option>
+						<option value="Elizabeth_Elliot">Elizabeth Elliot</option>
+						<option value="Mary_Elliot_Musgrove">Mary Elliot Musgrove </option>
+						<option value="Charles_Musgrove">Charles Musgrove</option>
+						<option value="Louisa_Musgrove">Louisa Musgrove</option>
+						<option value="Henrietta_Musgrove">Henrietta Musgrove</option>
+						<option value="Lady_Russell">Lady Russell</option>
+						<option value="Admiral_Croft">Admiral Croft</option>
+						<option value="Sophia_Croft">Sophia Croft</option>
+						<option value="Captain_Harville">Captian Harville</option>
+						<option value="William_Elliot">William Elliot</option>
+						<option value="Mrs_Smith">Mrs. Smith </option>
+						<option value="Lady_Dalyrmple">Lady Dalyrmple</option>
+						<option value="Miss_Carteret">Miss Carteret</option>
+						<option value="Captain_Benwick">Captain Benwick</option>
+						<option value="Mrs_Penelope_Clay">Mrs. Penelope Clay</option>
+						<option value="John_Shepard">John Shepard</option>
+						<option value="Charles_Hayter">Charles Hayter</option> 
+						  
+			  		 	</select>
+	        </div>
+	        <button type="submit" class="btn btn-link">Submit</button>
+      	</form>
+      	</div>
+    	</div>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-		<div class="container" id="back">
-			
-		    <div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<h2>View a Character</h2>
-		    			<form role="form" action="profile.php" method="GET">
-							<div class="form-group">
-			  				  <label for="category">Character</label>
-			  				<select class="form-control" id="category" name="category">
-			      			  <option value="Anne_Elliot">Anne Elliot</option>
-			      			  <option value="Captain_Frederick_Wentworth">Capt. Fredrick Wentworth</option>
-			     		      <option value="Sir_Walter_Elliot">Sir Walter Elliot</option>
-							  <option value="Elizabeth_Elliot">Elizabeth Elliot</option>
-							  <option value="Mary_Elliot_Musgrove">Mary Elliot Musgrove </option>
-							  <option value="Charles_Musgrove">Charles Musgrove</option>
-							  <option value="Louisa_Musgrove">Louisa Musgrove</option>
-							  <option value="Henrietta_Musgrove">Henrietta Musgrove</option>
-							  <option value="Lady_Russell">Lady Russell</option>
-							  <option value="Admiral_Croft">Admiral Croft</option>
-							  <option value="Sophia_Croft">Sophia Croft</option>
-							  <option value="Captain_Harville">Captian Harville</option>
-							  <option value="William_Elliot">William Elliot</option>
-							  <option value="Mrs_Smith">Mrs. Smith </option>
-							  <option value="Lady_Dalyrmple">Lady Dalyrmple</option>
-							  <option value="Miss_Carteret">Miss Carteret</option>
-							  <option value="Captain_Benwick">Captain Benwick</option>
-							  <option value="Mrs_Penelope_Clay">Mrs. Penelope Clay</option>
-							  <option value="John_Shepard">John Shepard</option>
-							  <option value="Charles_Hayter">Charles Hayter</option> 
-							  
-			  		 	 	</select>
-		    				</div>
-							<button type="submit" class="btn btn-default">Go to profile</button>
-						</form>
-					</div>
-					<div class="col-md-6">
-						<h2>View Interactions</h2>
-		    			<form role="form" action="newsfeed.php" method="GET">
-							<div class="form-group">
-			  				  <label for="category">Character</label>
-			  				<select class="form-control" id="category" name="category">
-			      			  <option value="Anne_Elliot">Anne Elliot</option>
-			      			  <option value="Captain_Frederick_Wentworth">Capt. Fredrick Wentworth</option>
-			     		      <option value="Sir_Walter_Elliot">Sir Walter Elliot</option>
-							  <option value="Elizabeth_Elliot">Elizabeth Elliot</option>
-							  <option value="Mary_Elliot_Musgrove">Mary Elliot Musgrove </option>
-							  <option value="Charles_Musgrove">Charles Musgrove</option>
-							  <option value="Louisa_Musgrove">Louisa Musgrove</option>
-							  <option value="Henrietta_Musgrove">Henrietta Musgrove</option>
-							  <option value="Lady_Russell">Lady Russell</option>
-							  <option value="Admiral_Croft">Admiral Croft</option>
-							  <option value="Sophia_Croft">Sophia Croft</option>
-							  <option value="Captain_Harville">Captian Harville</option>
-							  <option value="William_Elliot">William Elliot</option>
-							  <option value="Mrs_Smith">Mrs. Smith </option>
-							  <option value="Lady_Dalyrmple">Lady Dalyrmple</option>
-							  <option value="Miss_Carteret">Miss Carteret</option>
-							  <option value="Captain_Benwick">Captain Benwick</option>
-							  <option value="Mrs_Penelope_Clay">Mrs. Penelope Clay</option>
-							  <option value="John_Shepard">John Shepard</option>
-							  <option value="Charles_Hayter">Charles Hayter</option> 
-							  
-			  		 	 	</select>
-		    				</div>
-							<button type="submit" class="btn btn-default">Go to profile</button>
-						</form>
-					</div>
-      
-      </div>
-    </div>
-	
+    <section class="mainpage">
+    	
+    </section>
 
+    <section class="footer">
+    	<div class="mdst">MDST 3703 Final Project</div>
+    	<div class="pers">Persuabook &copy; 2014 </div>
+    	<div class="team">Alex Harasty | Christina Mandell | Sarah Smaltz | Helen He | Laura Holshauser</div>
+    </section>
 	
   </body>
+
 </html>
--- profile.php
+++ profile.php
@@ -7,6 +7,8 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
     <title>Profile</title>
 	
@@ -17,7 +19,8 @@
 	<link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="grid.css" rel="stylesheet">
+    <!-- <link href="grid.css" rel="stylesheet"> -->
+    <link href="style.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -32,60 +35,84 @@
 
   <body>
 	  
-	  <body>
-	    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="back2">
-	      <div class="container">
-	        <div class="navbar-header">
-	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-	            <span class="sr-only">Toggle navigation</span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <a class="navbar-brand" href="#">Persuasion Book</a>
+    
+    <div class="navbar navbar-fixed-top" role="navigation" id="back">
+      <div class="container">
+        <div class="navbar-header">          
+          <a href="persuasion.php"><img class="logosmall" src="fb white.png"></a>
+        </div>
+        <form class="navbar-form navbar-right" role="form" action="profile.php" method="GET">
+	        <div class="form-group">
+	          <label for="category" class="charsearch"><i class="fa fa-search fa-lg"></i></label>
+			  		<select class="form-control" id="category" name="category">
+			  			<option selected disabled hidden value=''></option>
+			      		<option value="Anne_Elliot">Anne Elliot</option>
+  						<option value="Captain_Frederick_Wentworth">Capt. Fredrick Wentworth</option>
+			     		<option value="Sir_Walter_Elliot">Sir Walter Elliot</option>
+						<option value="Elizabeth_Elliot">Elizabeth Elliot</option>
+						<option value="Mary_Elliot_Musgrove">Mary Elliot Musgrove </option>
+						<option value="Charles_Musgrove">Charles Musgrove</option>
+						<option value="Louisa_Musgrove">Louisa Musgrove</option>
+						<option value="Henrietta_Musgrove">Henrietta Musgrove</option>
+						<option value="Lady_Russell">Lady Russell</option>
+						<option value="Admiral_Croft">Admiral Croft</option>
+						<option value="Sophia_Croft">Sophia Croft</option>
+						<option value="Captain_Harville">Captian Harville</option>
+						<option value="William_Elliot">William Elliot</option>
+						<option value="Mrs_Smith">Mrs. Smith </option>
+						<option value="Lady_Dalyrmple">Lady Dalyrmple</option>
+						<option value="Miss_Carteret">Miss Carteret</option>
+						<option value="Captain_Benwick">Captain Benwick</option>
+						<option value="Mrs_Penelope_Clay">Mrs. Penelope Clay</option>
+						<option value="John_Shepard">John Shepard</option>
+						<option value="Charles_Hayter">Charles Hayter</option> 
+						  
+			  		 	</select>
 	        </div>
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			      <ul class="nav navbar-nav navbar-right">
-			        <li class="dropdown">
-			          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Setting<span class="caret"></span></a>
-			          <ul class="dropdown-menu" role="menu">
-			            <li><a href="">Main Page</a></li>
-			            <li><a href="">Something</a></li>
-			            <li><a href="">Something else</a></li>
-			          </ul>
-			        </li>
-			      </ul>
-			    </div><!-- /.navbar-collapse -->
-	      </div>
-	    </div>
+	        <button type="submit" class="btn btn-link">Submit</button>
+      	</form>
+      	</div>
+    </div>
 		
-	    <div id="nav"> 
+	<section class="profilepage">
 
-	    <?php
-	    // Create connection
-	    $topic = "http://studio1.shanti.virginia.edu/MDST3703F14/data/view/cmm4kd"; 
-	    $cat = $_GET['category'];
-		$req1 = include("$topic/about/code/$cat/full_name");
-		echo "$req1";
-	 	?>
-		<img src="<?php include("$topic/about/text/code/$cat/img_url"); ?>" alt="profile picture" id="prof_pic">
-				
+	<div class="col-md-3 col-md-offset-1">
+
+		    <?php
+		    // Create connection
+		    $topic = "http://studio1.shanti.virginia.edu/MDST3703F14/data/view/cmm4kd"; 
+		    $cat = $_GET['category'];
+			$req1 = include("$topic/about/code/$cat/full_name");
+			echo "$req1";
+		 	?>
+			<img src="<?php include("$topic/about/text/code/$cat/img_url"); ?>" alt="profile picture" class="prof_pic">
+					 
+	</div>		
+	  	
+	<div class="col-md-5">	
+		<div class="profilename"><?php include("$topic/about/text/code/$cat/full_name"); ?></div>
+	
+	    <div class="box">
+			<ul class="nav nav-tabs">
+			  <li role="presentation" class="active"><a href="#timeline">Timeline</a></li>
+			  <li role="presentation"><a href="#">About Me</a></li>
+			  <li role="presentation"><a href="#">Photos</a></li>
+			</ul>
+
+			<div id="timeline">
+				hi
+			</div>
+			
+	    </div>
 	</div>
-		
-		<h3 id="section"><?php include("$topic/about/text/code/$cat/full_name"); ?></h3>
-  
-		
-  	
-		
-    <div class="box">
-		<ul class="nav nav-tabs">
-		  <li role="presentation" class="active"><a href="#">Timeline</a></li>
-		  <li role="presentation"><a href="#">About Me</a></li>
-		  <li role="presentation"><a href="#">Photos</a></li>
-		</ul>
-      </div>
-	  
-  	
+
+	</section>
+
+  	<section class="footer">
+    	<div class="mdst">MDST 3703 Final Project</div>
+    	<div class="pers">Persuabook &copy; 2014 </div>
+    	<div class="team">Alex Harasty | Christina Mandell | Sarah Smaltz | Helen He | Laura Holshauser</div>
+    </section>
 	 
 
 
