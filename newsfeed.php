4e4034dbb2689c92153162deb2c8e7fb9eae48e4
avh2ag <alex.harasty@gmail.com>
2014-12-02 11:24:57 -0500

    Create Newsfeed.php
    Also links the persuasion nav to the newsfeed.

---
+++ newsfeed.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Profile</title>
+	
+
+
+      <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="grid.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+
+  <body>
+    <?php
+
+    $con="http://studio1.shanti.virginia.edu/MDST3703F14/data/view/cmmd4k/about/code/";
+
+    $cat = $_GET['category'];
+
+    ?>
+	  
+	    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="back2">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#">Persuasion Book</a>
+	        </div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav navbar-right">
+			        <li class="dropdown">
+			          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Setting<span class="caret"></span></a>
+			          <ul class="dropdown-menu" role="menu">
+			            <li><a href="">Main Page</a></li>
+			            <li><a href="">Something</a></li>
+			            <li><a href="">Something else</a></li>
+			          </ul>
+			        </li>
+			      </ul>
+			    </div><!-- /.navbar-collapse -->
+	      </div>
+	    
+		</div>
+		
+		
+	    <div id="nav" img src="" alt="profile picture" id="prof_pic">
+		
+	
+		<div id="box2"></div>
+		
+		
+	</div>
+		
+		
+		
+		<h3 id=""></h3>
+  
+		
+  	
+		
+    <div class="box">
+		<ul class="nav nav-tabs">
+		  <li role="presentation" class="active"><a href="#">Timeline</a></li>
+		  <li role="presentation"><a href="#">About Me</a></li>
+		  <li role="presentation"><a href="#">Photos</a></li>
+		</ul>
+      </div>
+	  
+  	
+	 
+
+
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
